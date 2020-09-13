fd7c399ce8efe44822665ba4a2b831e57c7eaace
Marcin Polak <mapoart@gmail.com>
2020-09-13 17:33:46 +0200

    Linux installers / commands

--- haskell.linux.nexss.config.js
+++ haskell.linux.nexss.config.js
@@ -14,4 +14,29 @@ languageConfig.compilers = {
   },
 };
 
+const {
+  replaceCommandByDist,
+  dist,
+} = require(`${process.env.NEXSS_SRC_PATH}/lib/osys`);
+
+const distName = dist();
+languageConfig.dist = distName;
+
+// TODO: Later to cleanup this config file !!
+switch (distName) {
+  case "Alpine Linux":
+    languageConfig.compilers.haskell.install = replaceCommandByDist(
+      "apk add ghc"
+    );
+    languageConfig.compilers.haskell.command = "ghc";
+    languageConfig.compilers.haskell.args =
+      "<file> && ./<fileNoExt> && rm ./<fileNoExt>";
+    break;
+  default:
+    languageConfig.compilers.haskell.install = replaceCommandByDist(
+      languageConfig.compilers.haskell.install
+    );
+    break;
+}
+
 module.exports = languageConfig;
