876b23e9fb8c06e2fdde9a099f4e8daa30a2bbbe
Marcin Polak <mapoart@gmail.com>
2023-05-07 17:31:32 +0200

    Non interactive

--- haskell.android.nexss.config.js
+++ haskell.android.nexss.config.js
@@ -2,14 +2,14 @@ let languageConfig = Object.assign({}, require("./haskell.win32.nexss.config"));
 languageConfig.compilers = {
   haskell: {
     install:
-      "apt install curl && curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh",
+      "export BOOTSTRAP_HASKELL_NONINTERACTIVE=1 apt install curl && curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh",
     command: "runhaskell",
     args: "<file>",
     help: ``,
   },
   stack: {
     install:
-      "apt update && apt install curl && curl -sSL https://get.haskellstack.org/ | sh",
+      "apt update && apt install curl && export BOOTSTRAP_HASKELL_NONINTERACTIVE=1 curl -sSL https://get.haskellstack.org/ | sh",
     command: "stack",
     args: "<file>",
     help: ``,
