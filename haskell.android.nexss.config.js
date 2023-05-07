18d05e743fb4b3a2d799c4d256fac50df824d043
Marcin Polak <mapoart@gmail.com>
2023-05-07 18:30:58 +0200

    Added correct install

--- haskell.android.nexss.config.js
+++ haskell.android.nexss.config.js
@@ -2,14 +2,14 @@ let languageConfig = Object.assign({}, require("./haskell.win32.nexss.config"));
 languageConfig.compilers = {
   haskell: {
     install:
-      "pkg install -y curl getconf && export BOOTSTRAP_HASKELL_NONINTERACTIVE=1 curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh",
+      "pkg install -y ghc",
     command: "runhaskell",
     args: "<file>",
     help: ``,
   },
   stack: {
     install:
-      "pkg install -y curl getconf && export BOOTSTRAP_HASKELL_NONINTERACTIVE=1 curl -sSL https://get.haskellstack.org/ | sh",
+      "pkg install -y ghc",
     command: "stack",
     args: "<file>",
     help: ``,
