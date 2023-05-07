dd4f0391e33b7c57f605987644a1b9395b022f0d
Marcin Polak <mapoart@gmail.com>
2023-05-07 17:35:30 +0200

    Added getconf install do Android platform support

--- haskell.android.nexss.config.js
+++ haskell.android.nexss.config.js
@@ -2,7 +2,7 @@ let languageConfig = Object.assign({}, require("./haskell.win32.nexss.config"));
 languageConfig.compilers = {
   haskell: {
     install:
-      "export BOOTSTRAP_HASKELL_NONINTERACTIVE=1 pkg install -y curl getconf && curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh",
+      "pkg install -y curl getconf && export BOOTSTRAP_HASKELL_NONINTERACTIVE=1 curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh",
     command: "runhaskell",
     args: "<file>",
     help: ``,
